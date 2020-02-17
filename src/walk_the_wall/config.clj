57939d10d04a66be6696ed512ba7e95e6c5a93d4
Rafael Ferreira <rafa@rafasf.com>
2020-02-16 18:19:49 -0600

    Create status priority from its flow
    Reads the flow described in the configuration, and creates the showing
    priority going from close to done towards not started.

---
+++ src/walk_the_wall/config.clj
@@ -0,0 +1,21 @@
+(ns walk-the-wall.config
+  (:require [clojure.java.io :as io]
+            [clojure.string :refer [lower-case trim]]
+            [clojure.edn :as edn]))
+
+(defn status-priority-from [status-flow]
+  (->> status-flow
+       reverse
+       (map (comp lower-case trim))
+       (map-indexed (fn [index status] [status index]))
+       (into {})))
+
+(defn- read-from-env [variable]
+  (System/getenv variable))
+
+(defn load-from [path]
+  (->> (io/resource path)
+       slurp
+       edn/read-string
+       (map #(update % :token read-from-env))
+       (map #(assoc % :status-priority (status-priority-from (get % :status-flow))))))
---
+++ test/walk_the_wall/config_test.clj
@@ -0,0 +1,21 @@
+(ns walk-the-wall.config-test
+  (:require [clojure.test :refer :all])
+  (:require [walk-the-wall.config :refer [load-from status-priority-from]]))
+
+(deftest status-priority-from-test
+  (testing "creates priority from end to beginning"
+    (let [status-flow ["To do" "Dev" "Done"]
+          status-priority (status-priority-from status-flow)]
+      (is (= {"done" 0 "dev" 1 "to do" 2}
+             status-priority)))))
+
+(deftest load-config-from-test
+  (testing "config creation"
+    (let [configs (load-from "walls.edn")
+          config (first configs)]
+      (is (= (assoc config :status-priority
+                           {"done" 0
+                            "dev" 1
+                            "ready dev" 2
+                            "to do" 3})
+             config)))))
