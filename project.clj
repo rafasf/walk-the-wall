7fed7fbf7b16c4b47a847c1bea591a27120141d2
Rafael Ferreira <rafa@rafasf.com>
2020-04-04 15:49:05 -0500

    Add instructions for release

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject walk-the-wall "0.1.0"
+(defproject walk-the-wall :lein-v
   :description "Walk the wall"
   :url "https://github.com/rafasf/walk-the-wall"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
 
   :plugins [[lein-cljfmt "0.6.4"]
-            [lein-eftest "0.5.9"]]
+            [lein-eftest "0.5.9"]
+            [com.roomkey/lein-v "7.2.0"]]
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.7.1"]
@@ -22,6 +23,11 @@
   :resource-paths ["resources"]
 
   :eftest {:multithread? :false}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["v" "update"]
+                  ["v" "push-tags"]
+                  ["deploy"]]
 
   :profiles {
              :test {:dependencies [[http-kit.fake "0.2.1"]]}}
